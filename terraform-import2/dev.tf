f8c7257068e496cb90ba397570f7fb53247003e6
oracle1545 <153964140+oracle1545@users.noreply.github.com>
2024-02-14 20:31:14 +0000

    Add files via upload
    IAM User import

---
+++ terraform-import2/dev.tf
@@ -0,0 +1,12 @@
+# __generated__ by Terraform
+# Please review these resources and move them into your main configuration files.
+
+# __generated__ by Terraform from "George"
+resource "aws_iam_user" "lb" {
+  force_destroy        = null
+  name                 = "George"
+  path                 = "/"
+  permissions_boundary = null
+  tags                 = {}
+  tags_all             = {}
+}
